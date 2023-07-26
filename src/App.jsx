a5f5c7056e265495977109e71b41ea3c58c403d2
MTharreq <mtharreq@gmail.com>
2023-07-26 11:19:27 +0700

    Akhirnya bisa konek ke Vite :)

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Welcome to My Channel</h1>
+      <h1>Greenvfo | Herbarium Tracker 🪴</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
